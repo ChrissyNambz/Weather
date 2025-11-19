985e0424947ddc8bd01585a753d821a534d005b2
ChrissyNambz <christine.nambatya@gmail.com>
2025-11-19 20:41:28 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="Container">
+        
       <h1>Hello Everyone!!!</h1>
+
+      <button className="btn btn-primary"> Christine </button>
 
       <footer>
         This project was coded by Christine, and is open sourced on {""}
@@ -13,9 +17,10 @@ function App() {
           target="_blank"
           rel="noreferrer"
         >
-         GitHub
+          GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
