f7f90c15dbe580a512f92514efba70c8dc477beb
ChrissyNambz <christine.nambatya@gmail.com>
2025-11-19 19:58:01 +0000

    Removed app files that weren't needed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Everyone!!!</p>
+      <p>Hello Everyone!!!</p>
 
-        <a
-          className="Git-link"
-          href="https://github.com/ChrissyNambz"
-          target="_blank"
-          rel="noreferrer"
-        >
-          My GitHub Repository
-        </a>
-      </header>
+      <a
+        className="Git-link"
+        href="https://github.com/ChrissyNambz"
+        target="_blank"
+        rel="noreferrer"
+      >
+        My GitHub Repository
+      </a>
     </div>
   );
 }
